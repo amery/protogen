35c16b987bd160689821a9069156b6762e771fac
Alejandro Mery <amery@jpi.io>
2023-08-13 19:12:12 +0000

    protoc-gen-dump: introduce tool to write the binary request to disc
    to be used for testing and debugging
    
    Signed-off-by: Alejandro Mery <amery@jpi.io>

---
+++ cmd/protoc-gen-dump/raw.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"bytes"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/reflect/protoreflect"
+
+	"github.com/amery/protogen/pkg/protogen"
+)
+
+func saveRawMessage(m protoreflect.ProtoMessage, name string) error {
+	f, err := os.Create(name)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	b, err := proto.Marshal(m)
+	if err != nil {
+		return err
+	}
+
+	_, err = bytes.NewBuffer(b).WriteTo(f)
+	return err
+}
+
+func saveRawRequest(gen *protogen.Plugin, name string) error {
+	dirName, fileName := filepath.Split(filepath.Clean(name))
+	switch {
+	case fileName == "":
+		return protogen.ErrInvalidName
+	case !strings.HasSuffix(fileName, ".pb"):
+		fileName += ".req.pb"
+	}
+
+	name = dirName + fileName
+	gen.Printf("writing binary request to %q", name)
+	return saveRawMessage(gen.Request(), name)
+}
+
+func saveRawFile(gen *protogen.Plugin, file *protogen.File) error {
+	name := file.Base() + ".req.pb"
+
+	gen.Printf("writing binary %q to %q", file.Name(), name)
+	return saveRawMessage(file.Proto(), name)
+}
