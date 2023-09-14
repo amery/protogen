263b78b6c20cc949ef5086f566f3fb640a7236b0
Alejandro Mery <amery@jpi.io>
2023-09-14 18:12:27 +0000

    protogen: add Wrap() helper to wrap errors with a hint
    the hint can be a formatted string and it's intended to
    provide extra context
    
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- pkg/protogen/errors.go
+++ pkg/protogen/errors.go
@@ -1,8 +1,48 @@
 package protogen
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+)
 
 var (
 	// ErrNotImplemented tells certain logic hasn't been implemented yet
 	ErrNotImplemented = errors.New("not implemented")
 )
+
+// WrappedError is a simple wrapped error container
+type WrappedError struct {
+	Hint string
+	Err  error
+}
+
+func (e WrappedError) Error() string {
+	switch {
+	case e.Hint == "":
+		return e.Err.Error()
+	case e.Err == nil:
+		return e.Hint
+	default:
+		return e.Hint + ": " + e.Err.Error()
+	}
+}
+
+func (e WrappedError) Unwrap() error {
+	return e.Err
+}
+
+// Wrap wraps an error with a formatted hint message
+func Wrap(err error, hint string, args ...any) *WrappedError {
+	if err == nil {
+		return nil
+	}
+
+	if len(args) > 0 {
+		hint = fmt.Sprintf(hint, args...)
+	}
+
+	return &WrappedError{
+		Err:  err,
+		Hint: hint,
+	}
+}
