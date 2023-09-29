78f74b22b472d4699b91e3b9db3c377d93cbd3e5
Alejandro Mery <amery@jpi.io>
2023-09-29 20:09:32 +0000

    styles: drop module until it can be justfied
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- pkg/styles/styles.go
+++
@@ -1,9 +0,0 @@
-// Package styles assists on composing names
-package styles
-
-// Style describes how we want to render names
-type Style struct {
-	// FilenameSingleDot tells only the last part of a filename should
-	// be delimited by a dot
-	FilenameSingleDot bool
-}
