c10464d394d36ad0e2757727db3d22abcb4a26ff
Alejandro Mery <amery@jpi.io>
2023-10-18 18:03:21 +0000

    text: move CutLastRune/CutLastFunc from protogen to text
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- pkg/protogen/help.go
+++ pkg/protogen/help.go
@@ -3,7 +3,8 @@ package protogen
 import (
 	"reflect"
 	"sort"
-	"strings"
+
+	"github.com/amery/protogen/pkg/text"
 )
 
 func optional2[T any](p *T, fallback T) (T, bool) {
@@ -57,30 +58,9 @@ func IsNil(vi any) bool {
 	}
 }
 
-// CutLastFunc slices s around the last match of a rune checker,
-// returning the text before and after it.
-// The found result reports whether there was a match.
-// If there is no match in s, cut returns "", s, false.
-func CutLastFunc(s string, f func(rune) bool) (before string, after string, found bool) {
-	i := strings.LastIndexFunc(s, f)
-	if i < 0 {
-		return "", s, false
-	}
-
-	return s[:i], s[i+1:], true
-}
-
-// CutLastRune slices s around the last instance of the given rune,
-// returning the text before and after it.
-// The found result reports whether the rune appears in s.
-// If the rune does not appear in s, cut returns "", s, false.
-func CutLastRune(s string, r rune) (before string, after string, found bool) {
-	return CutLastFunc(s, func(r2 rune) bool { return r == r2 })
-}
-
 // SplitName the last element of a dot delimited name
-func SplitName(fullname string) (before string, after string, found bool) {
-	return CutLastFunc(fullname, func(r rune) bool { return r == '.' })
+func SplitName(name string) (before string, after string, found bool) {
+	return text.CutLastFunc(name, func(r rune) bool { return r == '.' })
 }
 
 // Sort sorts a slice of pointers
--- pkg/text/cut.go
+++ pkg/text/cut.go
@@ -19,3 +19,24 @@ func CutSuffix(s, suffix string) (string, bool) {
 
 	return s[:len(s)-len(suffix)], true
 }
+
+// CutLastFunc slices s around the last match of a rune checker,
+// returning the text before and after it.
+// The found result reports whether there was a match.
+// If there is no match in s, cut returns "", s, false.
+func CutLastFunc(s string, f func(rune) bool) (before string, after string, found bool) {
+	i := strings.LastIndexFunc(s, f)
+	if i < 0 {
+		return "", s, false
+	}
+
+	return s[:i], s[i+1:], true
+}
+
+// CutLastRune slices s around the last instance of the given rune,
+// returning the text before and after it.
+// The found result reports whether the rune appears in s.
+// If the rune does not appear in s, cut returns "", s, false.
+func CutLastRune(s string, r rune) (before string, after string, found bool) {
+	return CutLastFunc(s, func(r2 rune) bool { return r == r2 })
+}
