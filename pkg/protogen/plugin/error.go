53519c3f7ee6364b660dbcbbd6c4deb52343e9f3
Alejandro Mery <amery@jpi.io>
2023-11-15 22:54:11 +0000

    plugin: extend ExitError to optionally wrap another error
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- pkg/protogen/plugin/error.go
+++ pkg/protogen/plugin/error.go
@@ -5,6 +5,7 @@ import "fmt"
 // ExitError is an error that is expected to be
 // handled directly via os.Exit(e.Code)
 type ExitError struct {
+	Err  error
 	Code int
 }
 
@@ -12,6 +13,8 @@ func (e *ExitError) Error() string {
 	var s string
 
 	switch {
+	case e.Err != nil:
+		s = fmt.Sprintf("%s (exit:%v)", e.Err.Error(), e.Code)
 	case e.Code == 0:
 		s = "OK"
 	default:
@@ -19,4 +22,17 @@ func (e *ExitError) Error() string {
 	}
 
 	return s
+}
+
+func (e *ExitError) Unwrap() error {
+	return e.Err
+}
+
+// WithExitCode wraps a fatal error in [ExitError] so callers
+// know how to [os.Exit]
+func WithExitCode(err error, code int) *ExitError {
+	return &ExitError{
+		Err:  err,
+		Code: code,
+	}
 }
